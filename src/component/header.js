bd4e05c78edcde5ecfdfcd7aad29790ba6264ebb
petesalcedo <peter.salcedo.diaz@gmail.com>
2022-01-14 01:45:37 -0500

    Adding react header and styling

--- src/component/header.js
+++ src/component/header.js
@@ -1,17 +1,14 @@
-import react from "react"
 import React from "react"
 
-
-function Header() {
-    return(
-        <header>
-            <nav className='main-header'>
-                <h1 className='main-header-title'>MemeGenerator</h1>
-                <h1 className='main-header-2title'>React Course</h1>
-             
-            </nav>
+export default function Header() {
+    return (
+        <header className="header">
+            <img 
+                src="./images/troll-face.png" 
+                className="header--image"
+            />
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">React Course - Project 3</h4>
         </header>
     )
 }
-
-export default Header
