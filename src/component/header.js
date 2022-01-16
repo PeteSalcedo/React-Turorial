b044e6dd59fd83a023924e78b9fc1af275589dd9
petesalcedo <peter.salcedo.diaz@gmail.com>
2022-01-15 23:54:33 -0500

    adding header

--- src/component/header.js
+++ src/component/header.js
@@ -1,10 +1,11 @@
 import React from "react"
+import logo from '../images/logo.svg'
 
 export default function Header() {
     return (
         <header className="header">
             <img 
-                src="./images/troll-face.png" 
+                src={logo} 
                 className="header--image"
             />
             <h2 className="header--title">Meme Generator</h2>
