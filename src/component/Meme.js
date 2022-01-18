bf026d80b0f1107801bb60fffb3b7d36fb10fc28
petesalcedo <peter.salcedo.diaz@gmail.com>
2022-01-17 23:51:25 -0500

    Api request

--- src/component/Meme.js
+++ src/component/Meme.js
@@ -1,130 +1,74 @@
 import React from "react"
 
-export default function Form() {
-    const [formData, setFormData] = React.useState(
-        {
-            firstName: "", 
-            lastName: "", 
-            email: "", 
-            comments: "", 
-            isFriendly: true,
-            employment: "",
-            favColor: ""
-        }
-    )
+export default function Meme() {
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg" 
+    })
+    const [allMemes, setAllMemes] = React.useState([])
     
-    function handleChange(event) {
-        const {name, value, type, checked} = event.target
-        setFormData(prevFormData => {
-            return {
-                ...prevFormData,
-                [name]: type === "checkbox" ? checked : value
-            }
-        })
+
+    
+    React.useEffect(asyc() => {
+        fetch("https://api.imgflip.com/get_memes")
+            .then(res => res.json())
+            .then(data => setAllMemes(data.data.memes))
+    }, [])
+    
+    
+    
+    
+    
+    function getMemeImage() {
+        const randomNumber = Math.floor(Math.random() * allMemes.length)
+        const url = allMemes[randomNumber].url
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: url
+        }))
+        
     }
     
-    function handleSubmit(event) {
-        event.preventDefault()
-        // submitToApi(formData)
-        console.log(formData)
+    function handleChange(event) {
+        const {name, value} = event.target
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value
+        }))
     }
     
     return (
-        <form onSubmit={handleSubmit}>
-            <input
-                type="text"
-                placeholder="First Name"
-                onChange={handleChange}
-                name="firstName"
-                value={formData.firstName}
-            />
-            <input
-                type="text"
-                placeholder="Last Name"
-                onChange={handleChange}
-                name="lastName"
-                value={formData.lastName}
-            />
-            <input
-                type="email"
-                placeholder="Email"
-                onChange={handleChange}
-                name="email"
-                value={formData.email}
-            />
-            <textarea 
-                value={formData.comments}
-                placeholder="Comments"
-                onChange={handleChange}
-                name="comments"
-            />
-            <input 
-                type="checkbox" 
-                id="isFriendly" 
-                checked={formData.isFriendly}
-                onChange={handleChange}
-                name="isFriendly"
-            />
-            <label htmlFor="isFriendly">Are you friendly?</label>
-            <br />
-            <br />
-            
-            <fieldset>
-                <legend>Current employment status</legend>
+        <main>
+            <div className="form">
                 <input 
-                    type="radio"
-                    id="unemployed"
-                    name="employment"
-                    value="unemployed"
-                    checked={formData.employment === "unemployed"}
+                    type="text"
+                    placeholder="Top text"
+                    className="form--input"
+                    name="topText"
+                    value={meme.topText}
                     onChange={handleChange}
                 />
-                <label htmlFor="unemployed">Unemployed</label>
-                <br />
-                
                 <input 
-                    type="radio"
-                    id="part-time"
-                    name="employment"
-                    value="part-time"
-                    checked={formData.employment === "part-time"}
+                    type="text"
+                    placeholder="Bottom text"
+                    className="form--input"
+                    name="bottomText"
+                    value={meme.bottomText}
                     onChange={handleChange}
                 />
-                <label htmlFor="part-time">Part-time</label>
-                <br />
-                
-                <input 
-                    type="radio"
-                    id="full-time"
-                    name="employment"
-                    value="full-time"
-                    checked={formData.employment === "full-time"}
-                    onChange={handleChange}
-                />
-                <label htmlFor="full-time">Full-time</label>
-                <br />
-            </fieldset>
-            <br />
-            
-            <label htmlFor="favColor">What is your favorite color?</label>
-            <br />
-            <select 
-                id="favColor" 
-                value={formData.favColor}
-                onChange={handleChange}
-                name="favColor"
-            >
-                <option value="red">Red</option>
-                <option value="orange">Orange</option>
-                <option value="yellow">Yellow</option>
-                <option value="green">Green</option>
-                <option value="blue">Blue</option>
-                <option value="indigo">Indigo</option>
-                <option value="violet">Violet</option>
-            </select>
-            <br />
-            <br />
-            <button>Submit</button>
-        </form>
+                <button 
+                    className="form--button"
+                    onClick={getMemeImage}
+                >
+                    Get a new meme image 🖼
+                </button>
+            </div>
+            <div className="meme">
+                <img src={meme.randomImage} className="meme--image" />
+                <h2 className="meme--text top">{meme.topText}</h2>
+                <h2 className="meme--text bottom">{meme.bottomText}</h2>
+            </div>
+        </main>
     )
 }
